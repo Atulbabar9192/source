6610151cf81ed0f17ad5c971a582bb80354beed7
Atulbabar9192 <70954272+Atulbabar9192@users.noreply.github.com>
2020-10-19 10:35:47 -0700

    user service file 1st create
    UserService TMS first

---
+++ repos/ToursSystem/ToursSystem/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TMS.Data;
+using TMS.Persistance;
+
+
+namespace TMS.Service
+
+{
+  public  class UserService
+    {
+       public List<User>GetAllUsers()
+
+        {
+            ToursDataContext context = new ToursDataContext();
+            return context.Users.ToList();
+            
+        }
+        public User GetById(int id)
+
+        {
+            ToursDataContext context = new ToursDataContext();
+            return context.Users.FirstOrDefault(b => b.Id == id);
+              
+        }
+        public User GetByEmail(string email)
+
+        {
+            ToursDataContext context = new ToursDataContext();
+            return context.Users.FirstOrDefault(b => b.Email == email);
+
+        }
+        public int AddUser(User user)
+        {
+            ToursDataContext context = new ToursDataContext();
+            context.Users.Add(user);
+            context.SaveChanges();
+            return user.Id;            
+
+        }
+
+
+    }
+}
