e52eee1f9e3bbd643ec208c43a25aa9b76d6f63d
Atulbabar9192 <70954272+Atulbabar9192@users.noreply.github.com>
2020-10-19 22:42:12 -0700

    user controler added
    Added First Controler

---
+++ repos/ToursSystem/ToursSystem/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TMS.Service;
+
+namespace TMS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public IActionResult GetAllUsers()
+        {
+            UserService service = new UserService();
+            return Ok(service.GetAllUsers()); 
+        }
+    }
+}
